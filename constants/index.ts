9deca38b53057f1238abeb3e63684f90a0c5508d
Arsenic-01 <vedbhor25@gmail.com>
2024-08-11 17:08:16 +0530

    ..

--- constants/index.ts
+++ constants/index.ts
@@ -1,32 +1,3 @@
-export const GenderOptions = ['Male', 'Female', 'Other'];
-
-export const ClientFormDefaultValues = {
-  firstName: '',
-  lastName: '',
-  email: '',
-  phone: '',
-  gender: 'Male' as Gender,
-  organizationName: '',
-  emergencyContactName: '',
-  emergencyContactNumber: '',
-  identificationDocument: [],
-  privacyConsent: false,
-};
-
-export const IdentificationTypes = [
-  'Birth Certificate',
-  "Driver's License",
-  'Medical Insurance Card/Policy',
-  'Military ID Card',
-  'National Identity Card',
-  'Passport',
-  'Resident Alien Card (Green Card)',
-  'Social Security Card',
-  'State ID Card',
-  'Student ID Card',
-  'Voter ID Card',
-];
-
 export const StatusIcon = {
   scheduled: '/assets/icons/check.svg',
   pending: '/assets/icons/pending.svg',
